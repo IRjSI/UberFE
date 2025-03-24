7a0db0d6ae757a3f9e4e1ae1d224899c55b32168
IRjSI <rajusuthar0077@gmail.com>
2025-03-24 14:41:49 +0530

    implement context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <img className='w-16 ml-8' src="src\assets\Uber_logo_2018.svg.png" alt="" />
         <div className='py-4 px-4 bg-white'>
             <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
-            <Link to={'/login'} className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-4'>Continue</Link>
+            <Link to={'/login'} className='flex items-center justify-center w-full bg-black text-white font-semibold py-3 rounded mt-4'>Continue</Link>
         </div>
       </div>
     </div>
